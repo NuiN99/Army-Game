2789ce844ba42fb34c9571141469035cad4a23ed
NuiN99 <endyverse@gmail.com>
2023-04-27 19:40:35 -0700

    small renames

--- Army Game/Assets/Scripts/TroopStats.cs
+++ Army Game/Assets/Scripts/TroopStats.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "BasicStats", menuName = "ScriptableObjects/NewEnemyStats")]
+[CreateAssetMenu(fileName = "TroopStats", menuName = "ScriptableObjects/NewTroopStats")]
 public class TroopStats : ScriptableObject
 {
     public LayerMask searchMask;
